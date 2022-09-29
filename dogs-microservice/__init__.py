7197226ef4fb1aa73292ad66aa1fb7d824ff2f4d
agusramirez@google.com <agusramirez@google.com>
2022-09-29 13:35:30 -0500

    refactoring

--- dogs-microservice/__init__.py
+++ dogs-microservice/__init__.py
@@ -8,5 +8,15 @@ app.register_blueprint(pug.bp)
 def error_page():
     numerator = 0
     denominator = 0
+    app.logger.error('An error occurred: page not found')
     print( numerator / denominator ) # <=== This line raise a ZeroDivisionError
     return "Application Error"
+    
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=8080
+        #port=8443
+        #ssl_context='adhoc'
+        )
